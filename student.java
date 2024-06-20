487f8498d90f97baf5d9409986c40ea303f7c908
klemca4 <mca4@kletech.ac.in>
2024-06-20 12:17:22 +0530

    committed

---
+++ student.java
@@ -0,0 +1,12 @@
+public class student{
+    int rollno;
+    string name;
+    public void studentdetails(){
+        this,name = name;
+        this.rollno = rollno;
+    }
+
+    public void main(string[] args){
+        studentdetails();
+    }
+}
